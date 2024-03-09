e1d5b4ed314c85b757e0a62cc19434e526aedd06
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-09 11:00:54 +0500

    second commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question no 6        Stripping Names
+let name001 = "\t\n Asif Khadim \n\t";
+console.log('with white spaces', name001);
+console.log('without white spaces', name001.trim());
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Question no 6        Stripping Names
+
+let name001: string = "\t\n Asif Khadim \n\t";
+console.log('with white spaces', name001);
+console.log('without white spaces', name001.trim());
